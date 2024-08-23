788210a6a0afbff3ab3325f13fcc5b9c5b94428c
Eduardo Victor <eduardo.vic.goncalves@gmail.com>
2024-08-22 22:41:06 -0300

    Criacao de miniframeworl dentro do blog_jinja

--- blog_jinja/wsgi.py
+++ blog_jinja/wsgi.py
@@ -1,26 +1,35 @@
-import cgi
 from database import conn
-# NEW
-from jinja2 import Environment, FileSystemLoader
-# NEW
-env = Environment(loader=FileSystemLoader("template"))
+from rex import Rex
 
-def add_new_post(post):
-    cursor = conn.cursor()
-    cursor.execute(
-        """\
-        INSERT INTO post (title, content, author)
-        VALUES (:title, :content, :author);
-        """,
-        post,
-    )
-    conn.commit()
+app = Rex()
 
 
-# NEW
-def render_template(template_name, **context):
-    template = env.get_template(template_name)
-    return template.render(**context).encode("utf-8")
+@app.route("^/$", template="list.template.html")
+def post_list():
+    posts = get_posts_from_database()
+    return {"post_list": posts}
+
+@app.route("^/api$", template="list.template.html")
+def post_list_api():
+    posts = get_posts_from_database()
+    return {"post_list": posts}, "200 OK", application/json
+
+@app.route(r"^/(?P<id>\d{1,})$", template="post.template.html")
+def post_detail(id):
+    post = get_posts_from_database(post_id=id)[0]
+    return {"post": post}
+
+
+@app.route("^/new$", template="form.template.html")
+def new_post_form():
+    return {}
+
+
+@app.route("^/new$", method="POST")
+def new_post_add(form):
+    post = {item.name: item.value for item in form.list}
+    add_new_post(post)
+    return "New post Created with Success!", "201 Created", "text/plain"
 
 
 def get_posts_from_database(post_id=None):
@@ -35,48 +44,17 @@ def get_posts_from_database(post_id=None):
     return [dict(zip(fields, post)) for post in results]
 
 
-def application(environ, start_response):
-    path = environ["PATH_INFO"]
-    method = environ["REQUEST_METHOD"]
-    body = b"Content Not Found"
-    status = "404 Not Found"
-
-    if path == "/" and method == "GET":
-        posts = get_posts_from_database()
-        body = render_template(
-            "list.template.html",
-            post_list=posts  # NEW
-        )
-        status = "200 OK"
-
-    elif path.split("/")[-1].isdigit() and method == "GET":
-        post_id = path.split("/")[-1]
-        body = render_template(
-            "post.template.html",
-            post=get_posts_from_database(post_id=post_id)[0],
-        )
-        status = "200 OK"
-
-    elif path == "/new" and method == "POST":
-        form = cgi.FieldStorage(
-            fp=environ["wsgi.input"], environ=environ, keep_blank_values=1
-        )
-        post = {item.name: item.value for item in form.list}
-        add_new_post(post)
-        body = b"New post Created with Success!"
-        status = "201 Created"
-
-    elif path == "/new" and method == "GET":
-        body = render_template("form.template.html")
-        status = "200 OK"
-
-    headers = [("Content-type", "text/html")]
-    start_response(status, headers)
-    return [body]
+def add_new_post(post):
+    cursor = conn.cursor()
+    cursor.execute(
+        """\
+        INSERT INTO post (title, content, author)
+        VALUES (:title, :content, :author);
+        """,
+        post,
+    )
+    conn.commit()
 
 
 if __name__ == "__main__":
-    from wsgiref.simple_server import make_server
-
-    server = make_server("0.0.0.0", 8000, application)
-    server.serve_forever()
+    app.run()

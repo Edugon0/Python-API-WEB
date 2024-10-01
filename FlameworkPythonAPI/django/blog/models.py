deedb48c327e0ce428d00b21cc2f79998004febf
Eduardo Victor <eduardo.vic.goncalves@gmail.com>
2024-10-01 15:09:54 -0300

    FIX: Implementação de MVT no Django(incompleto)

--- FlameworkPythonAPI/django/blog/models.py
+++ FlameworkPythonAPI/django/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title= models.CharField(max_length=255)
+    slug= models.SlugField(max_length=255, unique=True)
+    content= models.TextField()
+    published= models.BooleanField(default=True)
+    date= models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["-date"]
--- FlameworkPythonAPI/django/djblog/urls.py
+++ FlameworkPythonAPI/django/djblog/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import new_post, PostDetail, PostList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("new/", new_post, name="new_post"),
+    path("", PostList.as_view(), name="index"),
+    path("<slug:slug>/", PostDetail.as_view(), name="detail"),
 ]
